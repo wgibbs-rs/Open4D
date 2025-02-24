75615f70d18b8fdc55614593c1c47a2fa15a9216
DischordDynne <86694203+GameBakerStudio@users.noreply.github.com>
2025-02-24 09:28:18 -0500

    Transform

--- src/physics/OFD_rigidbody_c.h
+++ src/physics/OFD_rigidbody_c.h
@@ -5,8 +5,8 @@
 #define OFD_rigidbody_c_h_
 
 
-#include "OFD_transform_c.h"
 #include "../geometry/OFD_mesh_c.h"
+#include "OFD_transform_c.h"
 
 /** @brief Stored information on a given physics object. */
 typedef struct {
@@ -17,5 +17,7 @@ typedef struct {
 /** @brief Animates a rigidbody by a time interval (dt). */
 static inline void OFD_AnimateRigidbody(OFD_Rigidbody* a, OFD_Rigidbody other[], double dt);
 
+/** @brief Calculates the moment of inertia for a geometry, given that mass is uniformly distributed. */
+static inline double* OFD_UniformInertiaTensor(OFD_Mesh mesh);
 
 #endif // OFD_rigidbody_c_h_
