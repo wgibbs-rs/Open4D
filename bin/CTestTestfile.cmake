a999da06d70d08b5ad59b4fcdad9a36232bd8c31
DischordDynne <wgibbs08@outlook.com>
2025-07-13 17:43:33 -0500

    Removed `/bin/` contents

--- bin/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/willgibb848/Projects/c-cpp/Open4D
-# Build directory: /Users/willgibb848/Projects/c-cpp/Open4D/bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestOpen4D "/Users/willgibb848/Projects/c-cpp/Open4D/bin/Open4DTestsExec")
-set_tests_properties(TestOpen4D PROPERTIES  _BACKTRACE_TRIPLES "/Users/willgibb848/Projects/c-cpp/Open4D/CMakeLists.txt;37;add_test;/Users/willgibb848/Projects/c-cpp/Open4D/CMakeLists.txt;0;")
